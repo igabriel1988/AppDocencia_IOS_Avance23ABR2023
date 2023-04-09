09da9658f7154b4ad8998897db8a3d4d09ca4d54
Ivan <igabrielcorreopersonal2@gmail.com>
2023-04-08 17:57:54 -0700

    Initial Commit

---
+++ AppDocencia23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDocencia23
+//
+//  Created by Ivan Gabriel on 08/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
